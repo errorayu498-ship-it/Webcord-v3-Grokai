74dc99d1f81e491d355ecb282647a41ee03bde23
errorayu498-ship-it <errorayu498@gmail.com>
2026-03-19 13:10:47 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // config.php
 define('ADMIN_EMAIL', 'admin@webcord.com');
-define('ADMIN_PASSWORD', 'subhan123');     // change this immediately after first use
+define('ADMIN_PASSWORD', 'root8962');     // change this immediately after first use
 define('SECRET_KEY', 'change-this-to-strong-random-string-2025');  // used for simple JWT-like token
 define('CREDIT_PER_MINUTE', 2);
 define('TOOL_COST', 90);
